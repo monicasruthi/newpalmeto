eb3d5ef48abf41c7ca4dbcaa8e716c60784dedfd
monicasruthi <monicasruthi24@gmail.com>
2020-03-13 17:23:26 +0530

    Commit message

---
+++ JDBC/src/jdbc/Employee.java
@@ -0,0 +1,33 @@
+package jdbc;
+
+public class Employee {
+  
+	int id;
+	String firstname;
+	String lastname;
+	
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
+}
